238e39799fda716bf68463bbb0217e5ec9f87a2c
Jonathan Smith <hello@iamjonathan.me>
2025-12-16 15:51:09 +0000

    added vitest global types

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'src/test/'],
+      exclude: ['node_modules/'],
     },
   },
 })
